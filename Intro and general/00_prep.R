4a427eff5ae8a3096f6713560c3675534f75804c
Eline <e.krijkamp@erasmusmc.nl>
2020-02-01 14:23:59 +0100

    Update material for CE16

--- Intro and general/00_prep.R
+++ Intro and general/00_prep.R
@@ -1,29 +1,34 @@
-### Run all code in this script to download the desired R packages from either CRAN or GitHub
+# Welcome to the CE16 course
+
+### Run all code in this script to download the desired R packages 
+### from either CRAN or GitHub
 
 # first download and use this package to conveniently install other packages
 if (!require('pacman')) install.packages('pacman'); library(pacman) 
 
 # load (install if required) packages from CRAN
-p_load("here", "devtools", "dplyr", "scales", "ellipse", "ggplot2", "lazyeval", "igraph", 
-       "truncnorm", "ggraph", "reshape2", "knitr")
+p_load("here", "devtools", "dplyr", "scales", "ellipse", "ggplot2", "lazyeval", "igraph", "truncnorm", "ggraph", "reshape2", "knitr", "markdown")
 
 # load (install if required) packages from GitHub
-# install_github("DARTH-git/dampack", force = TRUE) Uncomment if there is a newer version
+install_github("DARTH-git/dampack", force = TRUE) # (Un)comment if there is a newer version
 p_load_gh("DARTH-git/dampack")
 
 
-### FOR INSTRUCTORS: Below packages are module-specific
+# DECISION TREE
+install_github("DARTH-git/dectree", force = TRUE) # (Un)comment if there is a newer version
+p_load_gh("DARTH-git/dectree")   
+
+
+### Below packages are module-specific are ar not relavent at this point. 
 
 # SURVIVAL ANALYSIS
-p_load("gems", "flexsurv", "survHE", "msm", "mstate")
+# p_load("gems", "flexsurv", "survHE", "msm", "mstate")
 
 # CALIBRATION
-p_load("lhs", "IMIS", "matrixStats", "plotrix", "psych", "scatterplot3d")
+# p_load("lhs", "IMIS", "matrixStats", "plotrix", "psych", "scatterplot3d")
 
 # VOI
-p_load("grid", "mgcv", "gridExtra", "gdata")
+#p_load("grid", "mgcv", "gridExtra", "gdata")
           
-# DECISION TREE
-# install_github("DARTH-git/dectree", force = TRUE) Uncomment if there is a newer version
-p_load_gh("DARTH-git/dectree")     
+  
           

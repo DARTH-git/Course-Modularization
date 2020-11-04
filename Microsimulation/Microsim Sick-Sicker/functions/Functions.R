9cbf294dc434fffa564a877aab2e9ae9413b992e
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2020-11-04 11:17:55 +0100

    Add update microsim code to the course modularisation

--- Microsimulation/Microsim Sick-Sicker/functions/Functions.R
+++ Microsimulation/Microsim Sick-Sicker/functions/Functions.R
@@ -86,3 +86,144 @@ gammaPar <- function(m, s) {
   scale <- s ^ 2 / m
   list(shape = shape, scale = scale)
 }
+
+#----------------------------------------------------------------------------#
+####   Function to check if transition probability array/matrix  is valid ####
+#----------------------------------------------------------------------------#
+#' Check if transition array is valid
+#'
+#' \code{check_transition_probability} checks if transition probabilities are in \[0, 1\].
+#'
+#' @param a_P A transition probability array or matrix.
+#' @param err_stop Logical variable to stop model run if set up as TRUE. Default = FALSE.
+#' @param verbose Logical variable to indicate print out of messages. 
+#' Default = FALSE
+#'
+#' @return
+#' This function stops if transition probability array is not valid and shows 
+#' what are the entries that are not valid
+#' @import utils
+#' @export
+check_transition_probability <- function(a_P,
+                                         err_stop = FALSE, 
+                                         verbose = FALSE) {
+  
+  a_P <- as.array(a_P)
+  m_indices_notvalid <- arrayInd(which(a_P < 0 | a_P > 1), 
+                                 dim(a_P))
+  
+  if(dim(m_indices_notvalid)[1] != 0){
+    v_rows_notval   <- rownames(a_P)[m_indices_notvalid[, 1]]
+    v_cols_notval   <- colnames(a_P)[m_indices_notvalid[, 2]]
+    v_cycles_notval <- dimnames(a_P)[[3]][m_indices_notvalid[, 3]]
+    
+    df_notvalid <- data.frame(`Transition probabilities not valid:` = 
+                                matrix(paste0(paste(v_rows_notval, v_cols_notval, sep = "->"),
+                                              "; at cycle ",
+                                              v_cycles_notval), ncol = 1), 
+                              check.names = FALSE)
+    
+    if(err_stop) {
+      stop("Not valid transition probabilities\n",
+           paste(capture.output(df_notvalid), collapse = "\n"))
+    }
+    
+    if(verbose){
+      warning("Not valid transition probabilities\n",
+              paste(capture.output(df_notvalid), collapse = "\n"))
+    } 
+  }
+}
+
+#----------------------------------------------------------------------------#
+####   Function to check if sum of transition probabilities equal to one  ####
+#----------------------------------------------------------------------------#
+#' Check if the sum of transition probabilities equal to one. 
+#'
+#' \code{check_sum_of_transition_array} checks if each of the rows of the 
+#' transition matrices sum to one. 
+#' 
+#' @param a_P A transition probability array.
+#' @param n_states Number of health states.
+#' @param n_t Number of cycles.
+#' @param err_stop Logical variable to stop model run if set up as TRUE. 
+#' Default = TRUE.
+#' @param verbose Logical variable to indicate print out of messages. 
+#' Default = TRUE
+#' @return 
+#' The transition probability array and the cohort trace matrix.
+#' @import dplyr
+#' @export
+check_sum_of_transition_array <- function(a_P,
+                                          n_states,
+                                          n_t,  
+                                          err_stop = TRUE, 
+                                          verbose  = TRUE) {
+  
+  a_P <- as.array(a_P)
+  d <- length(dim(a_P))
+  # For matrix
+  if (d == 2) {
+    valid <- sum(rowSums(a_P))
+    if (abs(valid - n_states)> 1e-04 ) {
+      if(err_stop) {
+        browser()
+        stop("This is not a valid transition Matrix")
+      }
+      
+      if(verbose){
+        warning("This is not a valid transition Matrix")
+      } 
+    }
+  } else {
+    # For array
+    valid <- (apply(a_P, d, function(x) sum(rowSums(x))) == n_states)
+    if (!isTRUE(all.equal(as.numeric(sum(valid)), as.numeric(n_t)))) {
+      if(err_stop) {
+        stop("This is not a valid transition Matrix")
+      }
+      
+      if(verbose){
+        warning("This is not a valid transition Matrix")
+      } 
+    }
+  }
+}
+
+
+#---------------------------------------------------------------------------#
+#### R function to sample states for multiple individuals simultaneously ####
+#---------------------------------------------------------------------------#
+
+# Krijkamp EM, Alarid-Escudero F, Enns EA, Jalal HJ, Hunink MGM, Pechlivanoglou P. 
+# Microsimulation modeling for health decision sciences using R: A tutorial. 
+# Med Decis Making. 2018;38(3):400–22. https://www.ncbi.nlm.nih.gov/pubmed/29587047
+
+################################################################################
+# Copyright 2017, THE HOSPITAL FOR SICK CHILDREN AND THE COLLABORATING INSTITUTIONS. 
+# All rights reserved in Canada, the United States and worldwide.  
+# Copyright, trademarks, trade names and any and all associated intellectual property 
+# are exclusively owned by THE HOSPITAL FOR SICK CHILDREN and the collaborating 
+# institutions and may not be used, reproduced, modified, distributed or adapted 
+# in any way without appropriate citation.
+
+#---------------------------------------------------------------------------#
+####                    R functions for visualization                    ####
+#---------------------------------------------------------------------------#
+
+
+# plot health state trace
+plot_m_TR <- function(m_M) {
+  # plot the distribution of the population across health states over time (trace)
+  # count the number of individuals in each health state at each cycle
+  m_TR <- t(apply(m_M, 2, function(x) table(factor(x, levels = v_n, ordered = TRUE)))) 
+  m_TR <- m_TR / n_i                                       # calculate the proportion of individuals 
+  colnames(m_TR) <- v_n                                    # name the rows of the matrix
+  rownames(m_TR) <- paste("Cycle", 0:n_t, sep = " ")       # name the columns of the matrix
+  # Plot trace of first health state
+  matplot(m_TR, type = "l", main = "Health state trace", col= 1:n_states,
+          ylim = c(0, 1), ylab = "Proportion of cohort", xlab = "Cycle")
+  legend("topright", v_n, col = 1:n_states,    # add a legend to current plot
+         lty = rep(1, 3), bty = "n", cex = 0.65)
+  
+}

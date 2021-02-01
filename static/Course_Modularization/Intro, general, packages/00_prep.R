2a5a1c23f8de08996678d39cfd8f61ec5dd0f175
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2021-02-01 08:38:30 +0100

    add slides

--- static/Course_Modularization/Intro, general, packages/00_prep.R
+++ static/Course_Modularization/Intro, general, packages/00_prep.R
@@ -14,7 +14,7 @@ p_load("devtools", "dplyr", "scales", "ellipse",
        "matrixStats", "lhs", "IMIS", "plotrix",
        "psych", "scatterplot3d", "grid", "mgcv", 
        "gridExtra", "gdata", "triangle", "flexsurv", "msm", 
-       "survminer", "gems") 
+       "survminer", "gems", "jsonlite", "rstudioapi", "tidyverse") 
 
 # Enter an empty line to skip updates when prompted
 install_version("ggplot2", version = "3.3.0", repos = "http://cran.us.r-project.org") 
@@ -23,11 +23,16 @@ install_version("ggplot2", version = "3.3.0", repos = "http://cran.us.r-project.
 p_load("ggrepel")
 
 # load (install if required) packages from GitHub
-#install_github("DARTH-git/dampack", force = TRUE) # (Un)comment if there is a newer version
+install_github("DARTH-git/dampack", force = TRUE) # (Un)comment if there is a newer version
 p_load_gh("DARTH-git/dampack", dependencies = F)
 
-#install_github("DARTH-git/darthtools", force = TRUE) # (Un)comment if there is a newer version
+install_github("DARTH-git/darthtools", force = TRUE) # (Un)comment if there is a newer version
 p_load_gh("DARTH-git/darthtools")
+
+install_github("DARTH-git/DARTH-git/OpenTree", force = TRUE) # (Un)comment if there is a newer version
+
+p_load_gh("DARTH-git/OpenTree")
+
 
 # Install additional packages
 p_load("ggraph") 

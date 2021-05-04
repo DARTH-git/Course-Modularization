7560df1ce2eb56fb47de4f22259c8a21566f2642
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2021-05-04 19:44:54 +0200

    Update how to install OpenTree

--- static/Course_Modularization/Intro, general, packages/00_prep.R
+++ static/Course_Modularization/Intro, general, packages/00_prep.R
@@ -1,11 +1,15 @@
 ### Run all code in this script to download the desired R packages
 ### from either CRAN or GitHub
 # The entire process will take several minutes
-# Recommend running the code line by line, in the specified order below
 
+
+#############################################################
+# Run the code LINE BY LINE , in the specified order below
+############################################################
 # first download and use this package to conveniently install other packages
 install.packages('pacman')
 library(pacman) 
+
 
 # load (install if required) packages from CRAN
 p_load("abind",  "dampack", "data.table", "DES", "devtools", "diagram", "dplyr", 
@@ -25,10 +29,10 @@ p_load("ggrepel")
 
 # load (install if required) packages from GitHub
 
-# install_github("DARTH-git/darthtools", force = TRUE) # (Un)comment if there is a newer version#
+install_github("DARTH-git/darthtools", force = TRUE) # (Un)comment if there is a newer version#
 p_load_gh("DARTH-git/darthtools")
 
-# install_github("DARTH-git/DARTH-git/OpenTree", force = TRUE) # (Un)comment if there is a newer version
+install_github("DARTH-git/OpenTree", force = TRUE) # (Un)comment if there is a newer version
 p_load_gh("DARTH-git/OpenTree")
 
 

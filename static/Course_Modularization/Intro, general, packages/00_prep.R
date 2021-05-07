b22d1798366aeb19cf11c95070d4acefece41fd5
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2021-05-07 12:00:34 +0200

    Remove IMIS from 00_prep.R

--- static/Course_Modularization/Intro, general, packages/00_prep.R
+++ static/Course_Modularization/Intro, general, packages/00_prep.R
@@ -14,7 +14,7 @@ library(pacman)
 # load (install if required) packages from CRAN
 p_load("abind",  "dampack", "data.table", "DES", "devtools", "diagram", "dplyr", 
        "ellipse", "flexsurv", "flexsurvcure", "gdata", "gems", "grid", "gridExtra", 
-       "igraph", "IMIS", "jsonlite", "knitr", "lazyeval", "lhs", 
+       "igraph", "jsonlite", "knitr", "lazyeval", "lhs", 
        "markdown", "matrixStats", "mgcv", "msm", "mstate",
        "plotrix", "purrr", "psych", "reshape2", "rstudioapi",   
        "scales", "scatterplot3d", "stringr", "survHE", "survminer", "shiny",

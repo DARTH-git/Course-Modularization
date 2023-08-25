d785d180f14da2fd8b6aa4e772bfeb0a840439c9
Alan <alanyang0924@gmail.com>
2023-08-25 15:51:35 -0400

    a

--- static/Course_Modularization/Sensitivity analysis/Markov Sick-Sicker SA/solutions/Functions_markov_sick-sicker_time.R
+++ static/Course_Modularization/Sensitivity analysis/Markov Sick-Sicker SA/solutions/Functions_markov_sick-sicker_time.R
@@ -94,11 +94,11 @@ decision_model <- function(l_params_all, verbose = FALSE) {
     
     ### Check if transition probability matrices are valid
     ## Check that transition probabilities are [0, 1]
-    check_transition_probability(a_P_SoC,   verbose = TRUE)
-    check_transition_probability(a_P_strAB, verbose = TRUE)
+    check_transition_probability(a_P_SoC,   verbose = verbose)
+    check_transition_probability(a_P_strAB, verbose = verbose)
     ### Check that all rows for each slice of the array sum to 1
-    check_sum_of_transition_array(a_P_SoC,   n_states = n_states, n_cycles = n_cycles, verbose = TRUE)
-    check_sum_of_transition_array(a_P_strAB, n_states = n_states, n_cycles = n_cycles, verbose = TRUE)
+    check_sum_of_transition_array(a_P_SoC,   n_states = n_states, n_cycles = n_cycles, verbose = verbose)
+    check_sum_of_transition_array(a_P_strAB, n_states = n_states, n_cycles = n_cycles, verbose = verbose)
     
     #### Run Markov model ####
     ## Iterative solution of age-dependent cSTM
